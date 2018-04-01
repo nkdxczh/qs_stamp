b0fa8f528193016a48ced400862f34971144216b
nkdxczh <zhc416@lehigh.edu>
2018-04-01 18:01:11 -0400

    final

--- tests/preprocess.sh
+++ tests/preprocess.sh
@@ -36,6 +36,40 @@ do
             op=0;
         fi
     fi
+done < 'output'
 
-done < "$1"
+time='old_time'
+tx='old_tx'
+conflict='old_conflict'
 
+echo "" > $time
+echo "" > $tx
+echo "" > $conflict
+
+alltx=0
+
+while IFS='' read -r line || [[ -n "$line" ]]; 
+do
+    if [[ ${line:0:1} == [0-9]* ]] ;
+    then 
+        #echo $line 
+
+        if [[ $op == 0 ]];
+        then
+            x=$line;
+            ((op++));
+        elif [[ $op == 1 ]];
+        then
+            echo $x $line >> $time
+            ((op++));
+        elif [[ $op == 2 ]];
+        then
+            echo $x $line >> $tx
+            ((op++));
+        elif [[ $op == 3 ]];
+        then
+            echo $x $line >> $conflict
+            op=0;
+        fi
+    fi
+done < 'old_output'
--- tests/test.sh
+++ tests/test.sh
@@ -1,17 +1,20 @@
 tries=5
-threads=3
-clusters=40
+threads=64
+clusters=15
 queues=2
 
 echo "" > output
+echo "" > old_output
 
 cd ..
-for v in {1..3..1}
+for v in {5..64..2}
 do
     echo $v
     echo $v >> tests/output
+    echo $v >> tests/old_output
     queues=$v
     #echo ./run.sh $tries $threads $clusters \"-DUSE_QS\" $queues
     ./run.sh $tries $threads $clusters \"-DUSE_QS\" $queues >> tests/output
+    ./run.sh $tries $threads $clusters "" >> tests/old_output
 done
 cd tests
